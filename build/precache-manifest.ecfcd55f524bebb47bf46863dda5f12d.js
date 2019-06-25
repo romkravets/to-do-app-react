4c2cdd2cc65c45fc0596e9581b924b1da2985757
romkravets <romann.kravets@gmail.com>
2019-06-25 19:03:58 +0300

    Added change repo to-do-app

---
+++ build/precache-manifest.ecfcd55f524bebb47bf46863dda5f12d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d343af4ade0c145710afcb4a2af596ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a00be932df72ca2be1e3",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b173f2de5d3af5235c30",
+    "url": "/static/js/2.5420db0f.chunk.js"
+  },
+  {
+    "revision": "a00be932df72ca2be1e3",
+    "url": "/static/js/main.0e2d5859.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
